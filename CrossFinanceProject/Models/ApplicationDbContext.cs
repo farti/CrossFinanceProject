5963d1d47375c792b7c50f39b3a7498ddd56347b
Marcin Rakoczy <mrakoczy@wp.pl>
2018-07-23 13:53:43 +0200

    Added Connection to DataBase

---
+++ CrossFinanceProject/Models/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MySql.Data.Entity;
+
+namespace CrossFinanceProject.Models
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext()
+            : base("WebAppCon")
+        {
+
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+    }
+
+
+}
